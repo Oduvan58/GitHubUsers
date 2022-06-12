c3055ce2db25059d2ab8a439aac489d32f20b92c
Oduvan58 <yancheuskipavel@gmail.com>
2022-06-12 19:51:20 +0300

    Add recyclerView and viewBinding

--- app/src/main/java/by/yancheuski/githubusers/MainActivity.kt
+++ app/src/main/java/by/yancheuski/githubusers/MainActivity.kt
@@ -2,10 +2,15 @@ package by.yancheuski.githubusers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.yancheuski.githubusers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
